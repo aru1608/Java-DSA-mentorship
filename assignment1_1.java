8411ee68d78674d6a9ea40c2a952cdd481e9af56
Arushree Mishra <144672722+aru1608@users.noreply.github.com>
2023-09-27 22:24:27 +0530

    Q1.1: PRINT Z

---
+++ assignment1_1.java
@@ -0,0 +1,9 @@
+package dsa;
+import java.util.*;
+public class assignment1_1 
+{
+    public static void main(String[] args)
+    {
+        System.out.print("*****\n    *\n   *\n  *\n *\n*****\n");
+    }
+}
